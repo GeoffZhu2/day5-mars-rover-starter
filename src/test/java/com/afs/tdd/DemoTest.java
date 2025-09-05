692fdc06a88fe03ef2b2d3bcd8d5565a667376ac
zhuhao <zhuhao4396@foxmail.com>
2025-09-05 19:52:11 +0800

    test: execute command M for all directions

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DemoTest {
     @Test
-    void should_move_when_executeCommand_given_M() {
+    void should_move_forward_north_when_executeCommand_given_M() {
         // Given
         Location location = new Location(0, 0, Direction.N);
         MarsRover marsRover = new MarsRover(location);
@@ -14,6 +14,48 @@ class DemoTest {
         marsRover.executeCommand(Command.M);
         // Then
         Location expectedLocation = new Location(0, 1, Direction.N);
+        assertEquals(expectedLocation.getLocationX(), marsRover.getLocation().getLocationX());
+        assertEquals(expectedLocation.getLocationY(), marsRover.getLocation().getLocationY());
+        assertEquals(expectedLocation.getDirection(), marsRover.getLocation().getDirection());
+    }
+
+    @Test
+    void should_move_forward_south_when_executeCommand_given_M() {
+        // Given
+        Location location = new Location(0, 0, Direction.S);
+        MarsRover marsRover = new MarsRover(location);
+        // When
+        marsRover.executeCommand(Command.M);
+        // Then
+        Location expectedLocation = new Location(0, -1, Direction.S);
+        assertEquals(expectedLocation.getLocationX(), marsRover.getLocation().getLocationX());
+        assertEquals(expectedLocation.getLocationY(), marsRover.getLocation().getLocationY());
+        assertEquals(expectedLocation.getDirection(), marsRover.getLocation().getDirection());
+    }
+
+    @Test
+    void should_move_forward_east_when_executeCommand_given_E() {
+        // Given
+        Location location = new Location(0, 0, Direction.E);
+        MarsRover marsRover = new MarsRover(location);
+        // When
+        marsRover.executeCommand(Command.M);
+        // Then
+        Location expectedLocation = new Location(1, 0, Direction.E);
+        assertEquals(expectedLocation.getLocationX(), marsRover.getLocation().getLocationX());
+        assertEquals(expectedLocation.getLocationY(), marsRover.getLocation().getLocationY());
+        assertEquals(expectedLocation.getDirection(), marsRover.getLocation().getDirection());
+    }
+
+    @Test
+    void should_move_forward_west_when_executeCommand_given_E() {
+        // Given
+        Location location = new Location(0, 0, Direction.W);
+        MarsRover marsRover = new MarsRover(location);
+        // When
+        marsRover.executeCommand(Command.M);
+        // Then
+        Location expectedLocation = new Location(-1, 0, Direction.W);
         assertEquals(expectedLocation.getLocationX(), marsRover.getLocation().getLocationX());
         assertEquals(expectedLocation.getLocationY(), marsRover.getLocation().getLocationY());
         assertEquals(expectedLocation.getDirection(), marsRover.getLocation().getDirection());

8b7c3ef8c790d5dbe436e13dd1f6af92a6407eea
Geoff Zhu <zhuge2@oocl.com>
2025-09-05 15:49:38 +0800

    test: execute command M

---
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -0,0 +1,20 @@
+package com.afs.tdd;
+
+public class MarsRover {
+    private Location location;
+    public MarsRover(Location location) {
+        this.location = location;
+    }
+
+    public void executeCommand(Command command) {
+
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+}
--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -2,9 +2,20 @@ package com.afs.tdd;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class DemoTest {
     @Test
-    void write_your_first_test() {
-
+    void should_move_when_executeCommand_given_M() {
+        // Given
+        Location location = new Location(0, 0, Direction.N);
+        MarsRover marsRover = new MarsRover(location);
+        // When
+        marsRover.executeCommand(Command.M);
+        // Then
+        Location expectedLocation = new Location(0, 1, Direction.N);
+        assertEquals(expectedLocation.getLocationX(), marsRover.getLocation().getLocationX());
+        assertEquals(expectedLocation.getLocationY(), marsRover.getLocation().getLocationY());
+        assertEquals(expectedLocation.getDirection(), marsRover.getLocation().getDirection());
     }
 }

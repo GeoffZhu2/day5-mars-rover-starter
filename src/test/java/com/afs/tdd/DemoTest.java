9c41b7d209cdc40ee32f4c6e64502d286d324695
zhuhao <zhuhao4396@foxmail.com>
2025-09-06 14:22:01 +0800

    test: execute command L for all directions

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -52,4 +52,53 @@ class DemoTest {
         Location expectedLocation = new Location(-1, 0, Direction.W);
         assertEquals(location, expectedLocation);
     }
+
+    @Test
+    void should_turn_to_west_when_executeCommand_given_L() {
+        // Given
+        Location location = new Location(0, 0, Direction.N);
+        MarsRover marsRover = new MarsRover(location);
+        // When
+        marsRover.executeCommand(Command.L);
+        // Then
+        Location expectedLocation = new Location(0, 0, Direction.W);
+        assertEquals(location, expectedLocation);
+    }
+
+    @Test
+    void should_turn_to_east_when_executeCommand_given_L() {
+        // Given
+        Location location = new Location(0, 0, Direction.S);
+        MarsRover marsRover = new MarsRover(location);
+        // When
+        marsRover.executeCommand(Command.L);
+        // Then
+        Location expectedLocation = new Location(0, 0, Direction.E);
+        assertEquals(location, expectedLocation);
+    }
+
+    @Test
+    void should_turn_to_north_when_executeCommand_given_L() {
+        // Given
+        Location location = new Location(0, 0, Direction.E);
+        MarsRover marsRover = new MarsRover(location);
+        // When
+        marsRover.executeCommand(Command.L);
+        // Then
+        Location expectedLocation = new Location(0, 0, Direction.N);
+        assertEquals(location, expectedLocation);
+    }
+
+    @Test
+    void should_turn_to_south_when_executeCommand_given_L() {
+        // Given
+        Location location = new Location(0, 0, Direction.W);
+        MarsRover marsRover = new MarsRover(location);
+        // When
+        marsRover.executeCommand(Command.L);
+        // Then
+        Location expectedLocation = new Location(0, 0, Direction.S);
+        assertEquals(location, expectedLocation);
+    }
+
 }
